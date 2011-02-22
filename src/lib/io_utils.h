08201ad87568752f12141dc35e5591bfe05aca52
Olga Yakovleva <yakovleva.o.v@gmail.com>
2011-02-22 22:29:55 +0300

    Under Windows assume that file names are in UTF-8 instead of the default ANSI encoding

---
+++ src/lib/io_utils.h
@@ -0,0 +1,25 @@
+/* Copyright (C) 2011  Olga Yakovleva <yakovleva.o.v@gmail.com> */
+
+/* This program is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef IO_UTILS_H
+#define IO_UTILS_H
+#include <stdio.h>
+#include <unitypes.h>
+
+#define UEOF 0xffffffffu
+
+ucs4_t ufgetc(FILE *f,uint8_t *d,size_t *l);
+FILE *my_fopen(const char *path,const char *mode);
+#endif
