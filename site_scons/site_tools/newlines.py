521a89bce31919ef05ad54b64d1866d54db1c5fd
Olga Yakovleva <yakovleva.o.v@gmail.com>
2011-08-08 21:06:22 +0400

    Under Windows install configuration examples

--- site_scons/site_tools/newlines.py
+++ site_scons/site_tools/newlines.py
@@ -15,5 +15,16 @@ def ConvertNewlines(target,source,env):
         f_in.close()
     return None
 
+def ConvertNewlinesB(target,source,env):
+    for t,s in zip(target,source):
+        f_in=open(str(s),"rb")
+        f_out=open(str(t),"wb")
+        f_out.write("\xef\xbb\xbf")
+        f_out.write(f_in.read().replace("\n","\r\n"))
+        f_out.close()
+        f_in.close()
+    return None
+
 def generate(env):
     env["BUILDERS"]["ConvertNewlines"]=Builder(action=ConvertNewlines,suffix=".txt")
+    env["BUILDERS"]["ConvertNewlinesB"]=Builder(action=ConvertNewlinesB,suffix=".txt")
