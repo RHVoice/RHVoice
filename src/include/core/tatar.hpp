3885b164e6ddb9a5c4f9e8e3ed39e9d99de53276
Olga Yakovleva <yakovleva.o.v@gmail.com>
2017-04-14 11:19:10 +0300

    Support for Tatar language

---
+++ src/include/core/tatar.hpp
@@ -0,0 +1,65 @@
+/* Copyright (C) 2017  Olga Yakovleva <yakovleva.o.v@gmail.com> */
+
+/* This program is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU Lesser General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU Lesser General Public License for more details. */
+
+/* You should have received a copy of the GNU Lesser General Public License */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef RHVOICE_TATAR_HPP
+#define RHVOICE_TATAR_HPP
+
+#include "str.hpp"
+#include "fst.hpp"
+#include "language.hpp"
+
+namespace RHVoice
+{
+  class tatar_info: public language_info
+  {
+  public:
+    tatar_info(const std::string& data_path,const std::string& userdict_path);
+
+    #ifdef WIN32
+    unsigned short get_id() const
+    {
+      return 1092;
+    }
+    #endif
+
+    std::string get_country() const
+    {
+      return "RUS/RU";
+    }
+
+  private:
+    smart_ptr<language> create_instance() const;
+  };
+
+  class tatar: public language
+  {
+  public:
+    explicit tatar(const tatar_info& info_);
+
+    const tatar_info& get_info() const
+    {
+      return info;
+    }
+
+    std::vector<std::string> get_word_transcription(const item& word) const;
+
+  private:
+    const tatar_info& info;
+    const fst g2p_fst;
+    const fst g2p_p_fst;
+    const fst lseq_fst;
+  };
+}
+#endif
