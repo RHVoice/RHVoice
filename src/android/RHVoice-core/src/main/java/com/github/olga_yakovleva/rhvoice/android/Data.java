eb2787fd3ffb0b8f2b53c3ec8638a60ccb02352a
Olga Yakovleva <yakovleva.o.v@gmail.com>
2017-12-29 17:17:08 +0300

    Android: add Azamat

--- src/android/RHVoice-core/src/main/java/com/github/olga_yakovleva/rhvoice/android/Data.java
+++ src/android/RHVoice-core/src/main/java/com/github/olga_yakovleva/rhvoice/android/Data.java
@@ -48,6 +48,7 @@ public final class Data
         lang.addVoice(new VoicePack("Natia", lang, 3, 0, "https://www.dropbox.com/s/jyqfza4156n0ccc/data.zip?dl=1"));
         addLanguage(lang);
         lang=new LanguagePack("Kyrgyz", "kir", "KGZ", 1, 10, "https://www.dropbox.com/s/8l87ibk99fajlfe/data.zip?dl=1");
+        lang.addVoice(new VoicePack("Azamat", lang, 3, 0, "https://www.dropbox.com/s/u0y0xqpcbrovtt6/data.zip?dl=1"));
         lang.addVoice(new VoicePack("Nazgul", lang, 3, 0, "https://www.dropbox.com/s/x8k4wsxzq3hjedx/data.zip?dl=1"));
         addLanguage(lang);
         lang=new LanguagePack("Russian", "rus", "RUS", 2, 2, "https://www.dropbox.com/s/hejp3zwzrv7oegy/data.zip?dl=1");
