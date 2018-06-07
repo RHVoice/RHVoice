9730cc2202fd7bae1346da621dc2d04210534e14
Olga Yakovleva <yakovleva.o.v@gmail.com>
2018-06-07 17:54:32 +0300

    Kyrgyz: add an entry to the spelling dictionary

--- src/android/RHVoice-core/src/main/java/com/github/olga_yakovleva/rhvoice/android/Data.java
+++ src/android/RHVoice-core/src/main/java/com/github/olga_yakovleva/rhvoice/android/Data.java
@@ -52,7 +52,7 @@ public final class Data
         lang=new LanguagePack("Georgian", "kat", "GEO", 1, 8, null, null);
         lang.addVoice(new VoicePack("Natia", lang, 4, 0, "http://blindaid.ge/files/RHVoice-voice-Georgian-Natia-v4.0.zip", null));
         addLanguage(lang);
-        lang=new LanguagePack("Kyrgyz", "kir", "KGZ", 1, 14, null, null);
+        lang=new LanguagePack("Kyrgyz", "kir", "KGZ", 1, 15, null, null);
         lang.addVoice(new VoicePack("Azamat", lang, 4, 0, null, null));
         lang.addVoice(new VoicePack("Nazgul", lang, 4, 0, null, null));
         addLanguage(lang);
