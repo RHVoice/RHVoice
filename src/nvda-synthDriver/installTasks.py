92dac14c475fd7bd4cb864541e229b9798f1dc1b
Olga Yakovleva <yakovleva.o.v@gmail.com>
2019-02-11 17:08:19 +0300

    NVDA addon: additional changes for compatibility with Python 3

--- src/nvda-synthDriver/installTasks.py
+++ src/nvda-synthDriver/installTasks.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2012  Olga Yakovleva <yakovleva.o.v@gmail.com>
+# Copyright (C) 2019  Beqa Gozalishvili <beqaprogger@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +21,10 @@ import shutil
 import config
 
 def onInstall():
-    module_dir=os.path.dirname(__file__.decode(sys.getfilesystemencoding()))
+    try:
+        module_dir=os.path.dirname(__file__.decode("mbcs"))
+    except AttributeError:
+        module_dir=os.path.dirname(__file__)
     config_dir=os.path.join(module_dir,"synthDrivers","RHVoice","config")
     user_config_dir=os.path.join(config.getUserDefaultConfigPath(),"RHVoice-config")
     if not os.path.isdir(user_config_dir):
