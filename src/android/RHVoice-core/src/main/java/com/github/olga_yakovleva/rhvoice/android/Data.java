3baadf0a463b9ae44dc7f6f88c6150203bb73f5f
Olga Yakovleva <yakovleva.o.v@gmail.com>
2019-08-07 16:31:30 +0300

    Fix a typo

--- src/android/RHVoice-core/src/main/java/com/github/olga_yakovleva/rhvoice/android/Data.java
+++ src/android/RHVoice-core/src/main/java/com/github/olga_yakovleva/rhvoice/android/Data.java
@@ -67,7 +67,7 @@ public final class Data
         lang.addVoice(new VoicePack("Anatol", lang, 4, 1, Checksums.VOICE_ANATOL, null, null));
         lang.addVoice(new VoicePack("Natalia", lang, 4, 0, Checksums.VOICE_NATALIA, null, null));
         addLanguage(lang);
-        lang=new LanguagePack("Brazilian-Portuguese", "por", "pt", "BRA", "BR", true, 1, 4, Checksums.LANGUAGE_BRAZILIAN_PORTUGUESE, "https://dl.bintray.com/olga-yakovleva/Data/RHVoice-F123-Brazilian-Portuguese-language-v1.7.zip", null);
+        lang=new LanguagePack("Brazilian-Portuguese", "por", "pt", "BRA", "BR", true, 1, 7, Checksums.LANGUAGE_BRAZILIAN_PORTUGUESE, "https://dl.bintray.com/olga-yakovleva/Data/RHVoice-F123-Brazilian-Portuguese-language-v1.7.zip", null);
         lang.addVoice(new VoicePack("leticia_f123", "Let\u00Edcia-F123", lang, 4, 3, Checksums.VOICE_LETICIA, "https://public.f123.org/download/RHVoice-Brazilian-Portuguese-voice-Leticia-F123-v4.3.zip", null));
         addLanguage(lang);
 }
