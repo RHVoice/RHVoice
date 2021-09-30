3127b09fb5c728dd9e72945f8ff6b7287d5942cb
Artem Plaksin <admin@maniyax.ru>
2021-09-30 06:35:29 +0300

    add Marianna (#400)
    New ukrainian female voice.

--- src/android/RHVoice-core/src/main/java/com/github/olga_yakovleva/rhvoice/android/Checksums.java
+++ src/android/RHVoice-core/src/main/java/com/github/olga_yakovleva/rhvoice/android/Checksums.java
@@ -48,6 +48,7 @@ final class Checksums
     public static final byte[] VOICE_TALGAT={-2, -85, -97, 61, -71, 7, 22, -113, 12, 63, 123, 50, -101, -27, -116, -122};
     public static final byte[] LANGUAGE_UKRAINIAN={4, 31, -123, -22, -96, 85, 39, 99, 32, -110, -23, -34, -10, -63, 92, -27};
     public static final byte[] VOICE_VOLODYMYR={-94, 29, 9, -45, 40, 122, 5, -92, -89, -5, 10, -63, -49, 76, 116, -112};
+    public static final byte[] VOICE_MARIANNA={-110, 43, 54, -18, -48, -126, 117, -42, 33, -4, -102, -69, -104, 74, 72, -11};
     public static final byte[] VOICE_ANATOL={62, -55, -8, -28, 29, 115, 55, -85, -60, 52, 126, -11, -103, -16, -72, 13};
     public static final byte[] VOICE_NATALIA={-20, -13, 29, -69, -124, -59, 104, 44, 86, -28, 13, -89, 38, 83, 34, -86};
     public static final byte[] LANGUAGE_BRAZILIAN_PORTUGUESE={-94, 29, 68, -74, -92, 110, -100, -28, -65, 74, 90, -64, -20, 32, -52, -20};
