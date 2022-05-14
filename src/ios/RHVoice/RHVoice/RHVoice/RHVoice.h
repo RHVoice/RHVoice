07fa4be5411e167252a562410384d13131296d7d
Ihor Shevchuk <ihorshev@gmail.com>
2022-05-14 20:33:28 +0300

    Added umbrella header to generate modulemap file that will allow to use this framework in Swift code.

---
+++ src/ios/RHVoice/RHVoice/RHVoice/RHVoice.h
@@ -0,0 +1,14 @@
+//
+//  RHVoice.h
+//  RHVoice
+//
+//  Created by Ihor Shevchuk on 14.05.2022.
+//
+
+#import <RHVoice/RHVoiceBridge.h>
+#import <RHVoice/RHVoiceBridgeParams.h>
+#import <RHVoice/RHVoiceLoggerProtocol.h>
+#import <RHVoice/RHSpeechUtterance.h>
+#import <RHVoice/RHSpeechSynthesizer.h>
+#import <RHVoice/RHSpeechSynthesisVoice.h>
+
