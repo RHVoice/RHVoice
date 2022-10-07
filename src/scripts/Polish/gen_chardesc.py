639fdb730c471c0e7819042fa45c69d357f4c0f4
Grzegorz Zlotowicz <grzezlo@wp.pl>
2022-10-07 19:19:33 +0200

    Polish language, rev. 12. Added reading of 85 punctuation symbols, 50 Hebrew letters, and few other. Improved reading of words: aquarius, aquilla, pizza. Properly read contractions: DLNA, UPNP, DNA, RNA, AK. Accent of words ending on "byśmy" i "byście".
    Co-authored-by @zstanecic

--- src/scripts/Polish/gen_chardesc.py
+++ src/scripts/Polish/gen_chardesc.py
@@ -2,8 +2,24 @@
 
 import os
 import sys
+import csv
 import unicodedata
 
+descriptionFragments={
+	"WITH ACUTE": "ostre",
+	"WITH BREVE": "z łuczkiem",
+	"WITH CARON": "z haczkiem",
+	"WITH CEDILLA AND ACUTE": "ostre z cedyllą",
+	"WITH CEDILLA": "z cedyllą",
+	"WITH CIRCUMFLEX": "z daszkiem",
+	"WITH GRAVE": "ciężkie",
+	"WITH MACRON": "z makronem",
+	"WITH RING ABOVE": "z kółkiem",
+	"with stroke": "skreślone",
+	"with tilde": "z tyldą",
+	"latin small letter ": "",
+	"latin capital letter ": "",
+} #descriptionFragments
 
 progname = os.path.basename(sys.argv[0])
 
@@ -23,12 +39,27 @@ else:
 
 if (scp < 1 or ecp < 1) or (ecp < scp):
 	sys.exit('Incorrect character range!')
+fi=open("chardesc.tsv", encoding='UTF-8')
+definedChars={}
+charsList=csv.reader(fi, delimiter="\t")
+next(charsList) #Skip header row.
+for ch in charsList:
+	lowcaseChar, upcaseChar, *_=ch
+	lowcaseChar=lowcaseChar.strip()
+	if len(lowcaseChar):
+		definedChars[lowcaseChar]=True
+	upcaseChar=upcaseChar.strip()
+	if len(upcaseChar):
+		definedChars[upcaseChar]=True
+fi.close()
 
-with open('chardesc.tsv', 'a', encoding='UTF-8') as f:
+with open('chardescnew.tsv', 'w', encoding='UTF-8') as f:
 	if f.tell() == 0:
 		f.write('Lower case\tUpper case\tTranslit\tspell\tNotes\n')
 	for i in range(scp, ecp+1):
 		ch = chr(i)
+		if ch in definedChars:
+			continue
 		if ch.isupper():
 			continue
 		chname = unicodedata.name(ch, '')
@@ -37,4 +68,7 @@ with open('chardesc.tsv', 'a', encoding='UTF-8') as f:
 		uch = ch.upper()
 		if len(uch) > 1:
 			uch = ch
+		chname=chname.lower()
+		for k in descriptionFragments.keys():
+			chname=chname.replace(k.lower(), descriptionFragments[k].lower())
 		f.write(f'{ch}\t{uch}\t\t\t{chname}\n')
