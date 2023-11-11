731491d9f1381305852b75d285da957a6e990428
Olga Yakovleva <olga@rhvoice.org>
2023-11-11 20:09:21 +0400

    Add missing copyright and license statements to two new files.

--- src/core/english_id.cpp
+++ src/core/english_id.cpp
@@ -1,3 +1,19 @@
+/* Copyright (C) 2023  Olga Yakovleva <olga@rhvoice.org> */
+
+/* This program is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU Lesser General Public License as published by */
+/* the Free Software Foundation, either version 2.1 of the License, or */
+/* (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU Lesser General Public License for more details. */
+
+/* You should have received a copy of the GNU Lesser General Public License */
+/* along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+
 #include "core/english_id.hpp"
 #include <iostream>
 namespace RHVoice
